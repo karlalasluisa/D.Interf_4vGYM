5316ceb51a9b1172da9b282260d34ac23c1e40af
Inero-dev <yytutu81@gmail.com>
2025-01-02 19:39:36 +0100

    Creación de los 2 componentes principales

---
+++ src/app/monitors/monitors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monitors',
+  imports: [],
+  templateUrl: './monitors.component.html',
+  styleUrl: './monitors.component.scss'
+})
+export class MonitorsComponent {
+
+}
