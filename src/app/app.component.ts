768a830fa991af904a79e91e9e61a95d19387f1f
karla lasluisa <karla.lasluisa@gmail.com>
2025-01-02 19:20:38 +0100

    Creado componente selector funcion
    debo cambiar un poco el diseño, pero la funcionalidad va bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FunctionSelectorComponent } from './function-selector/function-selector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent],
+  imports: [NavbarComponent, FunctionSelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '4vGYMAppDI';
+  currentView: string = 'activities'; // Vista inicial
+
+  updateView(view: string): void {
+    this.currentView = view;
+  }
 }
