df298cfce8ab6164e7fdeb3c7d6056d7b7aec616
karla lasluisa <karla.lasluisa@gmail.com>
2025-01-02 18:39:27 +0100

    Creación component selector-function

---
+++ src/app/function-selector/function-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-function-selector',
+  imports: [],
+  templateUrl: './function-selector.component.html',
+  styleUrl: './function-selector.component.scss'
+})
+export class FunctionSelectorComponent {
+
+}
