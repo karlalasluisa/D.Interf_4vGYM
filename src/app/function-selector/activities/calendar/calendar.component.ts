73ddc220e67fe48608e6b22c9eaca7951aa1a474
Inero-dev <yytutu81@gmail.com>
2025-01-07 20:47:53 +0100

    Acabado el calendario y en funcionamiento (sin ligar a el resto de la página)

--- src/app/function-selector/activities/calendar/calendar.component.ts
+++ src/app/function-selector/activities/calendar/calendar.component.ts
@@ -1,23 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
+import {ChangeDetectionStrategy, Component, model} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 
 @Component({
   standalone: true,
   selector: 'app-calendar',
-  imports: [MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatNativeDateModule,
-    FormsModule, CommonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
-  selectedDate: Date | null = null;
+  selected = model<Date | null>(null);
 }
