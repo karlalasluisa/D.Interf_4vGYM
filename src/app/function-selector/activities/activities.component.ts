a835db0051e7356bec5995898b3fc33da5a09035
Inero-dev <yytutu81@gmail.com>
2025-01-08 14:06:53 +0100

    Plantilla Actividades (repasar nombres)

--- src/app/function-selector/activities/activities.component.ts
+++ src/app/function-selector/activities/activities.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CalendarComponent } from './calendar/calendar.component';
+import { ActivityComponent } from './activity/activity.component';
 
 @Component({
   selector: 'app-activities',
-  imports: [CalendarComponent],
+  imports: [CalendarComponent, ActivityComponent],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
