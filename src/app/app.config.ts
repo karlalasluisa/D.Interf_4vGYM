ce891f67913767a6f8aa6b2fee622511dea20b3a
Inero-dev <yytutu81@gmail.com>
2025-01-10 16:50:27 +0100

    Eliminación de problemas con dependecias

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,7 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient()
   ]
 };
